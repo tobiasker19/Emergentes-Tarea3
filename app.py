e480f79abb827b2eeef010eb59136ce698a8f734
tobiasker19 <tobias.guerrero_c@mail.udp.cl>
2024-07-08 01:08:24 -0400

    v3

--- app.py
+++ app.py
@@ -5,3 +5,6 @@ from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
