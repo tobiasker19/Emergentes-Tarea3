0a4edb40fb0399877c43ae98e12558f72a77a3f2
tobiasker19 <tobias.guerrero_c@mail.udp.cl>
2024-07-08 00:51:37 -0400

    repo actualizado

--- models.py
+++ models.py
@@ -1,10 +1,4 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask import Flask
-from config import Config
-
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
+from app import db
 
 class Admin(db.Model):
     id = db.Column(db.Integer, primary_key=True)
